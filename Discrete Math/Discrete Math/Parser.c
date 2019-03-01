7394aca2160f5a06026a5e43c433b1ea4ea302cb
Joseph-Kocis <43280544+Joseph-Kocis@users.noreply.github.com>
2019-02-28 20:12:50 -0500

    Add files via upload

---
+++ Discrete Math/Discrete Math/Parser.c
@@ -0,0 +1,11 @@
+//
+//  Parser.c
+//  Discrete Math
+//
+//  Created by Jody Kocis on 2/28/19.
+//  Copyright © 2019 Joseph Kocis. All rights reserved.
+//
+
+#include <stdio.h>
+
+
---
+++ Discrete Math/Discrete Math/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Discrete Math
+//
+//  Created by Jody Kocis on 2/28/19.
+//  Copyright © 2019 Joseph Kocis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
